1a74edbe320aa13a196818fc73b7ed5097562918
John-Matthew Ferreira <jmatthewf97@gmail.com>
2025-01-30 21:28:51 +0200

    Fixing Variables

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 // Middleware
 app.use(express.json());
 app.use(express.static("public"));
