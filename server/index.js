bfbf75a0c2790d473fc7f9466130563e6330698b
John-Matthew Ferreira <jmatthewf97@gmail.com>
2025-01-30 21:31:51 +0200

    Fixing Variables

--- server/index.js
+++ server/index.js
@@ -46,10 +46,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Database connection error:', err));
 
